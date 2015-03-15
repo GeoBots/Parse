d8952891d5569d3eabace4f0fd82a306d7baaa3c
ksymer <ksymer@uw.edu>
2015-03-14 22:09:40 -0700

    added location perms, removed parse push functions, changed symerspace to mycompany

--- app/src/main/java/com/mycompany/parse/MainActivity.java
+++ app/src/main/java/com/mycompany/parse/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mycompany.parse;
+package com.mycompany.Parse;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -16,7 +16,6 @@ import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
-import com.symerspace.Parse.R;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
